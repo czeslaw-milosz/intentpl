82db05bcfa4cc7e9640e1089448b10f54e3130f9
Radoslaw Jurczak <radoslawjurczak95@gmail.com>
2023-06-26 20:12:27 +0200

    Quick fix (evaluation: str not callable)
    Signed-off-by: Radoslaw Jurczak <radoslawjurczak95@gmail.com>

--- intentpl/eval/eval_utils.py
+++ intentpl/eval/eval_utils.py
@@ -5,4 +5,5 @@ import numpy as np
 def compute_metrics(eval_pred, metric):
     predictions, labels = eval_pred
     predictions = np.argmax(predictions, axis=1)
-    return metric.compute(predictions=predictions, references=labels)
+    metric_fun = evaluate.load(metric)
+    return metric_fun.compute(predictions=predictions, references=labels)
